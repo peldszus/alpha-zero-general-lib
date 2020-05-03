1c0a2205a17d05a48f3eeec2c0a2c73eb6b6d75f
Andreas Peldszus <andreas.peldszus@posteo.de>
2020-05-03 20:44:35 +0200

    Enable pre-commit-hooks
    * Apply black formatting
    * Add flake8 and bugbear
    * Use isort and seed-isort-config

--- src/alpha_zero_general/Arena.py
+++ src/alpha_zero_general/Arena.py
@@ -1,11 +1,11 @@
-import numpy as np
 from tqdm import tqdm
-import time
 
-class Arena():
+
+class Arena:
     """
     An Arena class where any 2 agents can be pit against each other.
     """
+
     def __init__(self, player1, player2, game, display=None):
         """
         Input:
@@ -37,25 +37,34 @@ class Arena():
         curPlayer = 1
         board = self.game.getInitBoard()
         it = 0
-        while self.game.getGameEnded(board, curPlayer)==0:
-            it+=1
+        while self.game.getGameEnded(board, curPlayer) == 0:
+            it += 1
             if verbose:
-                assert(self.display)
+                assert self.display
                 print("Turn ", str(it), "Player ", str(curPlayer))
                 self.display(board)
-            action = players[curPlayer+1](self.game.getCanonicalForm(board, curPlayer))
+            action = players[curPlayer + 1](
+                self.game.getCanonicalForm(board, curPlayer)
+            )
 
-            valids = self.game.getValidMoves(self.game.getCanonicalForm(board, curPlayer),1)
+            valids = self.game.getValidMoves(
+                self.game.getCanonicalForm(board, curPlayer), 1
+            )
 
-            if valids[action]==0:
+            if valids[action] == 0:
                 print(action)
-                assert valids[action] >0
+                assert valids[action] > 0
             board, curPlayer = self.game.getNextState(board, curPlayer, action)
         if verbose:
-            assert(self.display)
-            print("Game over: Turn ", str(it), "Result ", str(self.game.getGameEnded(board, 1)))
+            assert self.display
+            print(
+                "Game over: Turn ",
+                str(it),
+                "Result ",
+                str(self.game.getGameEnded(board, 1)),
+            )
             self.display(board)
-        return curPlayer*self.game.getGameEnded(board, curPlayer)
+        return curPlayer * self.game.getGameEnded(board, curPlayer)
 
     def playGames(self, num, verbose=False):
         """
@@ -68,28 +77,28 @@ class Arena():
             draws:  games won by nobody
         """
 
-        num = int(num/2)
+        num = int(num / 2)
         oneWon = 0
         twoWon = 0
         draws = 0
         for _ in tqdm(range(num), desc="Arena.playGames (Player 1)"):
             gameResult = self.playGame(verbose=verbose)
-            if gameResult==1:
-                oneWon+=1
-            elif gameResult==-1:
-                twoWon+=1
+            if gameResult == 1:
+                oneWon += 1
+            elif gameResult == -1:
+                twoWon += 1
             else:
-                draws+=1
+                draws += 1
 
         self.player1, self.player2 = self.player2, self.player1
-        
+
         for _ in tqdm(range(num), desc="Arena.playGames (Player 2)"):
             gameResult = self.playGame(verbose=verbose)
-            if gameResult==-1:
-                oneWon+=1                
-            elif gameResult==1:
-                twoWon+=1
+            if gameResult == -1:
+                oneWon += 1
+            elif gameResult == 1:
+                twoWon += 1
             else:
-                draws+=1
+                draws += 1
 
         return oneWon, twoWon, draws
--- src/alpha_zero_general/Coach.py
+++ src/alpha_zero_general/Coach.py
@@ -1,26 +1,35 @@
+import os
+import sys
 from collections import deque
-from Arena import Arena
-from MCTS import MCTS
-import numpy as np
-from tqdm import tqdm
-import time, os, sys
-from pickle import Pickler, Unpickler
+from pickle import Pickler
+from pickle import Unpickler
 from random import shuffle
 
+import numpy as np
+from tqdm import tqdm
 
-class Coach():
+from .Arena import Arena
+from .MCTS import MCTS
+
+
+class Coach:
     """
     This class executes the self-play + learning. It uses the functions defined
     in Game and NeuralNet. args are specified in main.py.
     """
+
     def __init__(self, game, nnet, args):
         self.game = game
         self.nnet = nnet
         self.pnet = self.nnet.__class__(self.game)  # the competitor network
         self.args = args
         self.mcts = MCTS(self.game, self.nnet, self.args)
-        self.trainExamplesHistory = []    # history of examples from args.numItersForTrainExamplesHistory latest iterations
-        self.skipFirstSelfPlay = False    # can be overriden in loadTrainExamples()
+        self.trainExamplesHistory = (
+            []
+        )  # history of examples from args.numItersForTrainExamplesHistory latest iterations
+        self.skipFirstSelfPlay = (
+            False  # can be overriden in loadTrainExamples()
+        )
 
     def executeEpisode(self):
         """
@@ -45,21 +54,26 @@ class Coach():
 
         while True:
             episodeStep += 1
-            canonicalBoard = self.game.getCanonicalForm(board,self.curPlayer)
+            canonicalBoard = self.game.getCanonicalForm(board, self.curPlayer)
             temp = int(episodeStep < self.args.tempThreshold)
 
             pi = self.mcts.getActionProb(canonicalBoard, temp=temp)
             sym = self.game.getSymmetries(canonicalBoard, pi)
-            for b,p in sym:
+            for b, p in sym:
                 trainExamples.append([b, self.curPlayer, p, None])
 
             action = np.random.choice(len(pi), p=pi)
-            board, self.curPlayer = self.game.getNextState(board, self.curPlayer, action)
+            board, self.curPlayer = self.game.getNextState(
+                board, self.curPlayer, action
+            )
 
             r = self.game.getGameEnded(board, self.curPlayer)
 
-            if r!=0:
-                return [(x[0],x[2],r*((-1)**(x[1]!=self.curPlayer))) for x in trainExamples]
+            if r != 0:
+                return [
+                    (x[0], x[2], r * ((-1) ** (x[1] != self.curPlayer)))
+                    for x in trainExamples
+                ]
 
     def learn(self):
         """
@@ -70,27 +84,38 @@ class Coach():
         only if it wins >= updateThreshold fraction of games.
         """
 
-        for i in range(1, self.args.numIters+1):
+        for i in range(1, self.args.numIters + 1):
             # bookkeeping
-            print('------ITER ' + str(i) + '------')
+            print("------ITER " + str(i) + "------")
             # examples of the iteration
-            if not self.skipFirstSelfPlay or i>1:
-                iterationTrainExamples = deque([], maxlen=self.args.maxlenOfQueue)
-    
+            if not self.skipFirstSelfPlay or i > 1:
+                iterationTrainExamples = deque(
+                    [], maxlen=self.args.maxlenOfQueue
+                )
+
                 for _ in tqdm(range(self.args.numEps), desc="Self Play"):
-                    self.mcts = MCTS(self.game, self.nnet, self.args)   # reset search tree
+                    self.mcts = MCTS(
+                        self.game, self.nnet, self.args
+                    )  # reset search tree
                     iterationTrainExamples += self.executeEpisode()
 
-                # save the iteration examples to the history 
+                # save the iteration examples to the history
                 self.trainExamplesHistory.append(iterationTrainExamples)
-                
-            if len(self.trainExamplesHistory) > self.args.numItersForTrainExamplesHistory:
-                print("len(trainExamplesHistory) =", len(self.trainExamplesHistory), " => remove the oldest trainExamples")
+
+            if (
+                len(self.trainExamplesHistory)
+                > self.args.numItersForTrainExamplesHistory
+            ):
+                print(
+                    "len(trainExamplesHistory) =",
+                    len(self.trainExamplesHistory),
+                    " => remove the oldest trainExamples",
+                )
                 self.trainExamplesHistory.pop(0)
             # backup history to a file
-            # NB! the examples were collected using the model from the previous iteration, so (i-1)  
-            self.saveTrainExamples(i-1)
-            
+            # NB! the examples were collected using the model from the previous iteration, so (i-1)
+            self.saveTrainExamples(i - 1)
+
             # shuffle examples before training
             trainExamples = []
             for e in self.trainExamplesHistory:
@@ -98,42 +123,65 @@ class Coach():
             shuffle(trainExamples)
 
             # training new network, keeping a copy of the old one
-            self.nnet.save_checkpoint(folder=self.args.checkpoint, filename='temp.pth.tar')
-            self.pnet.load_checkpoint(folder=self.args.checkpoint, filename='temp.pth.tar')
+            self.nnet.save_checkpoint(
+                folder=self.args.checkpoint, filename="temp.pth.tar"
+            )
+            self.pnet.load_checkpoint(
+                folder=self.args.checkpoint, filename="temp.pth.tar"
+            )
             pmcts = MCTS(self.game, self.pnet, self.args)
-            
+
             self.nnet.train(trainExamples)
             nmcts = MCTS(self.game, self.nnet, self.args)
 
-            print('PITTING AGAINST PREVIOUS VERSION')
-            arena = Arena(lambda x: np.argmax(pmcts.getActionProb(x, temp=0)),
-                          lambda x: np.argmax(nmcts.getActionProb(x, temp=0)), self.game)
+            print("PITTING AGAINST PREVIOUS VERSION")
+            arena = Arena(
+                lambda x: np.argmax(pmcts.getActionProb(x, temp=0)),
+                lambda x: np.argmax(nmcts.getActionProb(x, temp=0)),
+                self.game,
+            )
             pwins, nwins, draws = arena.playGames(self.args.arenaCompare)
 
-            print('NEW/PREV WINS : %d / %d ; DRAWS : %d' % (nwins, pwins, draws))
-            if pwins+nwins == 0 or float(nwins)/(pwins+nwins) < self.args.updateThreshold:
-                print('REJECTING NEW MODEL')
-                self.nnet.load_checkpoint(folder=self.args.checkpoint, filename='temp.pth.tar')
+            print(
+                "NEW/PREV WINS : %d / %d ; DRAWS : %d" % (nwins, pwins, draws)
+            )
+            if (
+                pwins + nwins == 0
+                or float(nwins) / (pwins + nwins) < self.args.updateThreshold
+            ):
+                print("REJECTING NEW MODEL")
+                self.nnet.load_checkpoint(
+                    folder=self.args.checkpoint, filename="temp.pth.tar"
+                )
             else:
-                print('ACCEPTING NEW MODEL')
-                self.nnet.save_checkpoint(folder=self.args.checkpoint, filename=self.getCheckpointFile(i))
-                self.nnet.save_checkpoint(folder=self.args.checkpoint, filename='best.pth.tar')                
+                print("ACCEPTING NEW MODEL")
+                self.nnet.save_checkpoint(
+                    folder=self.args.checkpoint,
+                    filename=self.getCheckpointFile(i),
+                )
+                self.nnet.save_checkpoint(
+                    folder=self.args.checkpoint, filename="best.pth.tar"
+                )
 
     def getCheckpointFile(self, iteration):
-        return 'checkpoint_' + str(iteration) + '.pth.tar'
+        return "checkpoint_" + str(iteration) + ".pth.tar"
 
     def saveTrainExamples(self, iteration):
         folder = self.args.checkpoint
         if not os.path.exists(folder):
             os.makedirs(folder)
-        filename = os.path.join(folder, self.getCheckpointFile(iteration)+".examples")
+        filename = os.path.join(
+            folder, self.getCheckpointFile(iteration) + ".examples"
+        )
         with open(filename, "wb+") as f:
             Pickler(f).dump(self.trainExamplesHistory)
         f.closed
 
     def loadTrainExamples(self):
-        modelFile = os.path.join(self.args.load_folder_file[0], self.args.load_folder_file[1])
-        examplesFile = modelFile+".examples"
+        modelFile = os.path.join(
+            self.args.load_folder_file[0], self.args.load_folder_file[1]
+        )
+        examplesFile = modelFile + ".examples"
         if not os.path.isfile(examplesFile):
             print(examplesFile)
             r = input("File with trainExamples not found. Continue? [y|n]")
