18705393abf69aa5689856e565727c704ac59794
Andreas Peldszus <andreas.peldszus@posteo.de>
2020-05-03 22:17:59 +0200

    Add player classes (adapted from the Othello players)

--- src/alpha_zero_general/__init__.py
+++ src/alpha_zero_general/__init__.py
@@ -3,8 +3,21 @@ from .coach import Coach
 from .game import Game
 from .mcts import MCTS
 from .neuralnet import NeuralNet
+from .player import GreedyPlayer
+from .player import HumanPlayer
+from .player import RandomPlayer
 from .utils import dotdict
 
-__all__ = ["Arena", "Coach", "Game", "MCTS", "NeuralNet", "dotdict"]
+__all__ = [
+    "Arena",
+    "Coach",
+    "dotdict",
+    "Game",
+    "GreedyPlayer",
+    "HumanPlayer",
+    "MCTS",
+    "NeuralNet",
+    "RandomPlayer",
+]
 
 __version__ = "0.1.0"
