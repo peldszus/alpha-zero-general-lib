4950861ae118ad7360077e6b5601bc04c57c0867
Andreas Peldszus <andreas.peldszus@posteo.de>
2020-05-03 22:08:09 +0200

    Expose main classes in packe init

--- src/alpha_zero_general/__init__.py
+++ src/alpha_zero_general/__init__.py
@@ -1,1 +1,10 @@
+from .arena import Arena
+from .coach import Coach
+from .game import Game
+from .mcts import MCTS
+from .neuralnet import NeuralNet
+from .utils import dotdict
+
+__all__ = ["Arena", "Coach", "Game", "MCTS", "NeuralNet", "dotdict"]
+
 __version__ = "0.1.0"
